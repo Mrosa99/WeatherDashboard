0bfbeeac363f308319b498a49fa6e009a132f114
MiltonRosa <125097380+Mrosa99@users.noreply.github.com>
2025-02-23 16:49:57 -0500

    State inclusion
    API can now recognize state/country to display weather data

--- myweather/myweather/views.py
+++ myweather/myweather/views.py
@@ -1,7 +1,7 @@
 import os
 import requests
 from django.shortcuts import render
-from dotenv import load_dotenv, dotenv_values
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -11,19 +11,27 @@ def index(request):
     error_message = None
 
     if request.method == "POST":
-        city = request.POST.get("city")
+        city_state = request.POST.get("city-state")
+        if city_state:
+            city_state_parts = city_state.split(",")
+            if len(city_state_parts) == 2:
+                city = city_state_parts[0].strip()
+                state = city_state_parts[1].strip()
 
-        api_key = os.getenv("API_KEY")
+                # Make the API call using city and state
+                api_key = os.getenv("API_KEY")
+                url = f"http://api.openweathermap.org/data/2.5/weather?q={city},{state}&appid={api_key}&units=imperial"
 
-        url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=imperial"
+                response = requests.get(url)
 
-        response = requests.get(url)
-
-        if response.status_code == 200:
-            weather_data = response.json()
-            print(weather_data)
-        else:
-            error_message = "City not found or API issue."
+                if response.status_code == 200:
+                    weather_data = response.json()
+                else:
+                    error_message = "City not found or API issue."
+            else:
+                error_message = (
+                    "Please enter the city and state in 'City, State' format."
+                )
 
     return render(
         request,
