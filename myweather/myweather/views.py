642a0395970f550ee572afe7fa6b7bd82f5b1987
MiltonRosa <125097380+Mrosa99@users.noreply.github.com>
2025-02-23 16:40:45 -0500

    API Works
    API now is asble to retrieve data for specific city

--- myweather/myweather/views.py
+++ myweather/myweather/views.py
@@ -1,28 +1,32 @@
 import os
 import requests
 from django.shortcuts import render
-from dotenv import load_dotenv
+from dotenv import load_dotenv, dotenv_values
 
 load_dotenv()
 
 
 def index(request):
     weather_data = None
-    if request.method == "GET":
-        print("Form submitted!")
-        city = request.GET.get("city")
-        print(f"City entered: {city}")
+    error_message = None
+
+    if request.method == "POST":
+        city = request.POST.get("city")
+
         api_key = os.getenv("API_KEY")
-        url = (
-            f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
-        )
+
+        url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=imperial"
 
         response = requests.get(url)
+
         if response.status_code == 200:
             weather_data = response.json()
             print(weather_data)
-
         else:
-            weather_data = {"error": "City not found or API issue."}
+            error_message = "City not found or API issue."
 
-    return render(request, "index.html", {"weather_data": weather_data})
+    return render(
+        request,
+        "index.html",
+        {"weather_data": weather_data, "error_message": error_message},
+    )
