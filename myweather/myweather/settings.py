6aa23ecd9aaf85ddad2cecfab74204f4eccbfa8c
MiltonRosa <125097380+Mrosa99@users.noreply.github.com>
2025-02-24 17:36:51 -0500

    Collect static files for deployment

--- myweather/myweather/settings.py
+++ myweather/myweather/settings.py
@@ -115,6 +115,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+STATICFILES_DIRS = [
+    BASE_DIR / "myweather/static",  # Path to the static files directory
+]
 
 STATIC_URL = "static/"
 
